0f3f1b9b5ea24417b8d9036d4f057814a2d65bb0
GUILHERME DE SOUZA BORSI <guilherme.sborsi@senacsp.edu.br>
2022-12-06 21:53:25 -0300

    Adicionar arquivos de projeto.

---
+++ API_X/Controllers/Criação_produto.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_X.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Criação_produto : ControllerBase
+    {
+        [HttpGet]
+        public string List1(string p_nome1)
+
+        {
+            string Chaveconexao = "Data Source=10.39.45.44;Initial Catalog=TI_Noite;User ID=Turma2022;Password=Turma2022@2022";
+            DataSet resultadoCEP3 = new DataSet();
+
+            SqlConnection Conexao = new SqlConnection(Chaveconexao);
+            Conexao.Open();
+
+            string wQuery = $"select * from Produto where Data_Cadastro like '%{p_nome1}%' ";
+            SqlDataAdapter adapter = new SqlDataAdapter(wQuery, Conexao);
+            adapter.Fill(resultadoCEP3);
+
+            string json = JsonConvert.SerializeObject(resultadoCEP3, Formatting.Indented);
+
+            return json;
+        }
+    }
+}
